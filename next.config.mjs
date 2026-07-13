9d59824d4945d715163aa14d73b1b3ac1b427a7b
regisksc <41172158+regisksc@users.noreply.github.com>
2026-07-13 10:54:00 -0300

    chore: write next config

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import { execSync } from 'child_process'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+let commitSha = process.env.SOURCE_COMMIT || 'unknown'
+try {
+  commitSha = execSync('git rev-parse --short HEAD').toString().trim()
+} catch {}
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default nextConfig
